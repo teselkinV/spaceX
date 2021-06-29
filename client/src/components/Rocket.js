959bd181874379a3c7538cd57d93072c8052d924
Vladimir Tesiolchin <vladimir@MacBook-Pro-Vladimir.local>
2021-06-29 10:23:58 -0700

    Added first stage info

--- client/src/components/Rocket.js
+++ client/src/components/Rocket.js
@@ -23,6 +23,28 @@ const ROCKET_QUERY = gql`
         kg
         lb
       }
+      first_stage {
+        engines
+        fuel_amount_tons
+        burn_time_sec
+        reusable
+      }
+      second_stage {
+        engines
+        fuel_amount_tons
+        burn_time_sec
+        thrust {
+          kN
+          lbf
+        }
+      }
+      engines {
+        number
+        type
+        version
+        propellant_1
+        propellant_2
+      }
       landing_legs {
         number
         material
@@ -49,6 +71,10 @@ function Rocket(props) {
     height,
     diameter,
     mass,
+    first_stage,
+    second_stage,
+    engines,
+    landing_legs,
   } = data.rocket;
   return (
     <div>
@@ -57,6 +83,7 @@ function Rocket(props) {
           <div className="container root">
             <div>
               <h3 className="my-3 text-uppercase rocket-name">{rocket_name}</h3>
+              <p className="my-4 ">{description}</p>
               <h2 className="my-4 text-uppercase">Rocket Details</h2>
             </div>
             <div className="row">
@@ -71,28 +98,114 @@ function Rocket(props) {
                   <li className="border-bottom my-2 py-3">
                     Mass {mass.kg} / {mass.lb}
                   </li>
-                  <li className="border-bottom my-2 py-3">
-                    Diameter {diameter.meters} / {diameter.feet}
-                  </li>
-                  <li className="border-bottom my-2 py-3">
-                    Mass {mass.kg} / {mass.lb}
-                  </li>
                 </ul>
-
-                <Link to="/rockets" className="btn btn-secondary my-3">
-                  Back
-                </Link>
               </div>
             </div>
           </div>
         </Carousel.Item>
         <Carousel.Item interval={10000000}>
-          <h3>Second slide</h3>
+          <div className="container root">
+            <div>
+              <h3 className="my-3 text-uppercase rocket-name">{rocket_name}</h3>
+
+              <h2 className="my-4 text-uppercase">First stage</h2>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <ul className="list px-0">
+                  <li className="border-bottom my-2 py-3">
+                    Engines {first_stage.engines}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Fuel amount {first_stage.fuel_amount_tons}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Burn time {first_stage.burn_time_sec}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Reusable{" "}
+                    <span
+                      className={
+                        first_stage.reusable ? "text-success" : "text-danger"
+                      }
+                    >
+                      {first_stage.reusable ? "Yes" : "No"}
+                    </span>
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Landing legs {landing_legs.number}
+                  </li>
+                  {landing_legs.material ? (
+                    <li className="border-bottom my-2 py-3">
+                      Landing legs material {landing_legs.material}
+                    </li>
+                  ) : null}
+                </ul>
+              </div>
+            </div>
+          </div>
         </Carousel.Item>
         <Carousel.Item interval={10000000}>
-          <h1>Third</h1>
+          <div className="container root">
+            <div>
+              <h3 className="my-3 text-uppercase rocket-name">{rocket_name}</h3>
+
+              <h2 className="my-4 text-uppercase">Second stage</h2>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <ul className="list px-0">
+                  <li className="border-bottom my-2 py-3">
+                    Number of engines {second_stage.engines}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Fuel amount {second_stage.fuel_amount_tons}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Burn time {second_stage.burn_time_sec}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Thrust {second_stage.thrust.kN}kN /{" "}
+                    {second_stage.thrust.lbf}lbf
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item interval={10000000}>
+          <div className="container root">
+            <div>
+              <h3 className="my-3 text-uppercase rocket-name">{rocket_name}</h3>
+              <h2 className="my-4 text-uppercase">Engine Details</h2>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <ul className="list px-0">
+                  <li className="border-bottom my-2 py-3">
+                    Number of engines {engines.number}
+                  </li>
+                  <li className="border-bottom my-2 py-3 text-capitalize">
+                    Type {engines.type}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Version {engines.version}
+                  </li>
+                  <li className="border-bottom my-2 py-3 text-capitalize">
+                    First tank for propellant {engines.propellant_1}
+                  </li>
+                  <li className="border-bottom my-2 py-3">
+                    Second tank for propellant {engines.propellant_2}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </Carousel.Item>
       </Carousel>
+      <Link to="/rockets" className="btn btn-secondary my-3">
+        Back
+      </Link>
     </div>
   );
 }
